aa4c3fe499f7f1759d35a213fad1c82c2b645847
mc2019a <densabayev@gmail.com>
2024-07-13 23:50:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './app.module.css';
+import data from './data.json';
+import {useState} from "react";
+import { logDOM } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+	const [steps, setSteps] = useState(data);
+	const [activateIndex, setActivateIndex] = useState(0);
 
-export default App;
+
+	const onBackButtonClick = () => {
+		setActivateIndex(activateIndex - 1)
+	}
+
+	const onForwardButtonClick = () => {
+		setActivateIndex(activateIndex + 1)
+	}
+
+	const onStartButtonClick = () => {
+		setActivateIndex(0)
+	}
+
+	const getPosition = (a, b) =>  {
+		if (a > b) return styles['steps-item']
+		if (a === b) return styles['steps-item'] + " " + styles.done + " " + styles.active
+		if (a < b) return styles['steps-item'] + " " + styles.done
+	}
+
+	let ifOnFirstPage = activateIndex === 0;
+	let ifOnLastPage = activateIndex === steps.length - 1;
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activateIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map(({id, title}, index) => <li
+							key={id}
+							className={getPosition(index, activateIndex)}
+						>
+							<button className={styles['steps-item-button']} onClick={() => {setActivateIndex(index)}}>{index + 1}</button>
+							{title}
+						</li>)}
+
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button className={styles.button} onClick={onBackButtonClick} disabled={ifOnFirstPage}>
+							Назад
+						</button>
+						<button className={styles.button} onClick={onForwardButtonClick} hidden={ifOnLastPage}>
+							Далее
+						</button>
+						<button className={styles.button} onClick={onStartButtonClick} hidden={!ifOnLastPage}>
+							Начать сначала
+						</button>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
